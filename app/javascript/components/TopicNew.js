89d44ce3e67cada10b080a412e70f4ff1dbc6320
Henry Doan <henrydoan96@gmail.com>
2021-09-08 18:51:26 -0600

    topic index new create

---
+++ app/javascript/components/TopicNew.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const TopicNew = ({ sub, topic }) => {
+  const { id } = sub
+  const { title, desc } = topic
+  const defaultTitle = title ? title : "";
+  const defaultDesc = desc ? desc : "";
+  return (
+    <>
+      <h1>New Topic</h1>
+      <form action={`/subs/${id}/topics`} method="post">
+        <input
+          type="text"
+          defaultValue={defaultTitle}
+          name="topic[title]"
+          required
+          placeholder="Title"
+        />
+        <input
+          type="text"
+          defaultValue={defaultDesc}
+          name="topic[desc]"
+          required
+          placeholder="Description"
+        />
+        <button type="submit">Add</button>
+      </form>
+    </>
+  )
+}
+
+export default TopicNew;
